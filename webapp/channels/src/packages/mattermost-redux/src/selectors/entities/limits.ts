a615387254e8a5858a3803fbced0640bb862c51b
Mike Gillis <1334080+mgillis@users.noreply.github.com>
2024-06-20 23:18:09 -0400

    move Apache licensed dirs to blue

--- webapp/channels/src/packages/mattermost-redux/src/selectors/entities/limits.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-import type {ServerLimits} from '@mattermost/types/limits';
-import type {GlobalState} from '@mattermost/types/store';
-
-export function getServerLimits(state: GlobalState): ServerLimits {
-    return state.entities.limits.serverLimits;
-}
